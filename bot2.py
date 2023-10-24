4cf01aa6f6a5003a0c1526012f1c33d427efabfa
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-10-24 10:40:41 +0700

    9.4 Инлайн-кнопки. Задача 2

--- bot2.py
+++ bot2.py
@@ -4,11 +4,51 @@ import logging
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from config_data.config import Config, load_config
+from lexicon.lexicon_ru import LEXICON
 
 dp = Dispatcher()
 logger = logging.getLogger(__name__)
+
+# Функция для генерации инлайн-клавиатур "на лету"
+
+
+def create_inline_kb(width: int,
+                     *args: str,
+                     last_btn: str | None = None,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=LEXICON[button] if button in LEXICON else button,
+                callback_data=button
+            ))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button))
+
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=width)
+    # Добавляем в билдер последнюю кнопку, если она передана в функцию
+    if last_btn:
+        kb_builder.row(InlineKeyboardButton(
+            text=last_btn,
+            callback_data='last_btn'
+        ))
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
+
 
 # Создаем объекты инлайн-кнопок
 big_button_1 = InlineKeyboardButton(
@@ -22,14 +62,20 @@ big_button_2 = InlineKeyboardButton(
 )
 
 # Создаем объект инлайн-клавиатуры
-keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[big_button_1],
-                     [big_button_2]]
-)
+# keyboard = InlineKeyboardMarkup(inline_keyboard=[[big_button_1],[big_button_2]])
 
+# keyboard = create_inline_kb(2, last_btn='Последняя кнопка', BUTTONS)
+# keyboard = create_inline_kb(2, last_btn=None, b_1='1', b_2='2', b_3='3', b_4='4', b_5='5', b_6='Последняя кнопка')
+
+# keyboard = create_inline_kb(2, last_btn='Последняя кнопка', *BUTTONS)
+# keyboard = create_inline_kb(2, last_btn='Последняя кнопка', **BUTTONS)
+# keyboard = create_inline_kb(2, last_btn='Последняя кнопка', b_1='1', b_2='2', b_3='3', b_4='4', b_5='5')
+keyboard = create_inline_kb(3, 'but_1', 'but_2', 'but_3', 'but_4', 'but_5', last_btn='Последняя кнопка')
 
 # Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
 # с data 'big_button_1_pressed'
+
+
 @dp.callback_query(F.data == 'big_button_1_pressed')
 async def process_button_1_press(callback: CallbackQuery):
     if callback.message.text != 'Была нажата БОЛЬШАЯ КНОПКА 1':
@@ -51,6 +97,7 @@ async def process_button_2_press(callback: CallbackQuery):
         )
     await callback.answer()
 
+
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(
@@ -59,6 +106,8 @@ async def process_start_command(message: Message):
     )
 
 # Функция конфигурирования и запуска бота
+
+
 async def main():
     # Конфигурируем логирование
     logging.basicConfig(
